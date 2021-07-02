4bfc6f915b73bb0013c1d26aa9774b5fe40c600d
草鞋没号 <308487730@qq.com>
2021-07-02 16:54:02 +0800

    feat: transformIndexHtml

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,17 @@
 import path from 'path'
 import fs from 'fs'
-import { defineConfig, PluginOption } from 'vite'
+import { defineConfig, Plugin } from 'vite'
 import { createVuePlugin } from 'vite-plugin-vue2'
 import _ from 'lodash'
 import pkg from './package.json'
-
-// The first step coyp ’public/index.html‘ to root directory
-copyIndexHtml()
 
 export default defineConfig((env) => ({
   plugins: [
     createVuePlugin({
       jsx: true,
+    }),
+    transformIndexHtml({
+      template: path.join(__dirname, 'public/index.html'),
     }),
   ],
   resolve: {
@@ -27,39 +27,45 @@ export default defineConfig((env) => ({
   },
 }))
 
-function copyIndexHtml() {
-  let indexHtml = fs.readFileSync(path.join(__dirname, 'public/index.html'), 'utf8')
-  const compiled = _.template(indexHtml, { interpolate: /<%=([\s\S]+?)%>/g })
-  const date = new Date
-  const hms = date
-    .toLocaleTimeString()
-    .split(' ')[0]
-    .split(':')
-    .join('-')
-  const ymd = date
-    .toLocaleDateString()
-    .split('/')
-    .reverse()
-    .join('-')
+function transformIndexHtml(opts: { template: string }): Plugin {
+  const rootIndexHtml = path.join(__dirname, 'index.html')
 
-  indexHtml = compiled({
-    BASE_URL: '/',
-    htmlWebpackPlugin: {
-      options: {
-        title: pkg.name,
-      },
-    }
-  })
+  if (!fs.existsSync(rootIndexHtml)) {
+    // Ensure the index.html exists in root directory.
+    fs.symlinkSync(opts.template, rootIndexHtml)
+  }
 
-  indexHtml = indexHtml.split('\n')
-    .map(line => line.includes('</body>')
-      ? `    <script type="module" src="/src/main.js">${ymd}|${hms}</script>
+  return {
+    name: 'cxmh:transformIndexHtml',
+    transformIndexHtml() {
+      let indexHtml: string
+
+      try {
+        indexHtml = fs.readFileSync(opts.template, 'utf8')
+        const compiled = _.template(indexHtml, { interpolate: /<%=([\s\S]+?)%>/g })
+
+        indexHtml = compiled({
+          BASE_URL: '/',
+          htmlWebpackPlugin: {
+            options: {
+              title: pkg.name,
+            },
+          }
+        })
+
+        indexHtml = indexHtml.split('\n')
+          .map(line => line.includes('</body>')
+            ? `    <script type="module" src="/src/main.js"></script>
 ${line}`
-      : line
-    )
-    .join('\n')
+            : line
+          )
+          .join('\n')
 
-  fs.writeFileSync(path.join(__dirname, 'index.html'), indexHtml)
+      } catch (error) {
+        indexHtml = `<h2>${error}</h2>`
+      }
 
-  console.log('[vite.config.ts] public/index.html has copyed.')
+      return indexHtml
+    },
+  }
 }
