e85a4d12bbde7bf60c3cd389c50c0d7dc52ff34b
草鞋没号 <308487730@qq.com>
2021-09-07 17:38:48 +0800

    rrefactor: better transformIndexHtml

--- vite.config.ts
+++ vite.config.ts
@@ -37,39 +37,35 @@ export default defineConfig((env) => ({
 
 function transformIndexHtml(opts: {
   template: string
-  templateData?: Record<string, unknown>
+  templateData?: Record<string, any>
 }): Plugin {
-  const rootIndexHtml = path.join(__dirname, 'index.html')
-
-  if (!fs.existsSync(rootIndexHtml)) {
-    // Ensure the index.html exists in root directory.
-    fs.symlinkSync(opts.template, rootIndexHtml)
-  }
-
   return {
     name: 'cxmh:transformIndexHtml',
-    transformIndexHtml() {
-      let indexHtml: string
+    configureServer(server) {
+      server.middlewares.use(async (req, res, next) => {
+        // if not html, next it.
+        if (!req.url?.endsWith('.html') && req.url !== '/') {
+          return next()
+        }
+        let indexHtml = fs.readFileSync(opts.template, 'utf8')
+        try {
+          const compiled = _.template(indexHtml, { interpolate: /<%=([\s\S]+?)%>/g })
 
-      try {
-        indexHtml = fs.readFileSync(opts.template, 'utf8')
-        const compiled = _.template(indexHtml, { interpolate: /<%=([\s\S]+?)%>/g })
+          indexHtml = compiled(opts.templateData)
 
-        indexHtml = compiled(opts.templateData)
+          indexHtml = indexHtml.split('\n')
+            .map(line => line.includes('</body>')
+              ? `    <script type="module" src="/src/main.js"></script>
+  ${line}`
+              : line
+            )
+            .join('\n')
 
-        indexHtml = indexHtml.split('\n')
-          .map(line => line.includes('</body>')
-            ? `    <script type="module" src="/src/main.js"></script>
-${line}`
-            : line
-          )
-          .join('\n')
-
-      } catch (error) {
-        indexHtml = `<h2>${error}</h2>`
-      }
-
-      return indexHtml
+        } catch (error) {
+          indexHtml = `<h2>${error}</h2>`
+        }
+        res.end(indexHtml)
+      })
     },
   }
 }
